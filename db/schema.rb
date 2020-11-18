3319e7901d50182e2e9372fc07241f1577e8282a
Ubuntu <ubuntu@ip-172-31-25-4.ec2.internal>
2020-11-18 03:43:15 +0000

    add book

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_18_034237) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "number_of_pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
